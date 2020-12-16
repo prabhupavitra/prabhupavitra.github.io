2fce3532fcae715e841e09057178e9efc4d78c17
Pavitra Prabhu <50896210+prabhupavitra@users.noreply.github.com>
2020-12-16 09:49:20 -0600

    Add files via upload

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to my blog!",
+        "excerpt":"This is just a little blog I’m starting. I’ll aim to get something up once a week or so, something interesting and technical.   I hope you like it!   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/blog/welcome-to-jekyll/",
+        "teaser": null
+      },{
+        "title": "Movie Recommender system in R",
+        "excerpt":"Movie Recommender System in R Movie Recommender System in R Pavitra Prabhu 2020-12-15 Introduction MovieLens Dataset Loss Functions Mean Absolute Error Root Mean Squared Error Project Checklist Methods and Analysis Data Ingestion Create Helper Functions Exploratory Data Analysis Rating Movies Users Date Genres Release Year of the Movie Data Preparation...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/projects/movierecommender/",
+        "teaser": null
+      }]
