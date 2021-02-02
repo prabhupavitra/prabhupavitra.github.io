753da526408cfe9f53377424c64f6db8841f59dc
Pavitra Gajanana <pavitragajanana@Pavitras-MacBook-Air.local>
2021-02-02 14:52:41 -0600

    learnings updated

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,16 @@
 var store = [{
+        "title": "Visualizing Amounts - Finance",
+        "excerpt":"Visualizing Amounts in Finance Most commonly used visualizations to compare amounts include: 1. Bar Charts 2. Diverging Bar Charts 3. Stacked Bar Charts 4. Grouped Bar Charts (also called Clustered Bar Charts) 5. Animated Bar Charts 6. Dot Plots (also called Cleveland Dot Charts) 7. Bubble Charts 8. Heatmaps Others...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/learning/R_DataVisualization_VisualizingAmounts/",
+        "teaser": null
+      },{
+        "title": "Visualizing Distributions - Finance",
+        "excerpt":"Visualizing Distributions in Finance Analyzing and visualizing distributions is widely used in Data Science projects since we are interested in understanding the behaviour of a numeric variable. In other words, we would want to know the central tendency of a variable as well as the outliers. We would also be...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/learning/R_DataVisualization_VisualizingDistributions/",
+        "teaser": null
+      },{
         "title": "Welcome to my blog!",
         "excerpt":"This is just a little blog I’m starting. I’ll aim to get something up once a week or so, something interesting and technical.   I hope you like it!   ","categories": [],
         "tags": [],
@@ -6,7 +18,7 @@ var store = [{
         "teaser": null
       },{
         "title": "Movie Recommender system in R",
-        "excerpt":"&lt;!DOCTYPE html&gt; Movie Recommender System in R Movie Recommender System in R Introduction MovieLens Dataset Loss Functions Mean Absolute Error Root Mean Squared Error Project Checklist Methods and Analysis Data Ingestion Create Helper Functions Exploratory Data Analysis Rating Movies Users Date Genres Release Year of the Movie Data Preparation Model...","categories": [],
+        "excerpt":"Movie Recommender System in R Movie Recommender System in R Introduction MovieLens Dataset Loss Functions Mean Absolute Error Root Mean Squared Error Project Checklist Methods and Analysis Data Ingestion Create Helper Functions Exploratory Data Analysis Rating Movies Users Date Genres Release Year of the Movie Data Preparation Model Selection Linear...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/projects/movierecommender/",
         "teaser": null
